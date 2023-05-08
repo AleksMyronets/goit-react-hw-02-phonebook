e58fc054977e24bc373c2482f60df26f7de60353
AleksMyronets <mironec.kiev.1993@gmail.com>
2023-05-08 10:27:40 +0300

    Додавання компонентів та стилів

---
+++ src/components/App/App.jsx
@@ -0,0 +1,8 @@
+import { Component } from 'react';
+import { Contacts } from './Contacts';
+import { Filter } from './Filter';
+import { Forma } from './Forma';
+import { nanoid } from 'nanoid';
+import { ContainerForm } from "./App.styled"
+
+
